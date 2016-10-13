880282bfc19e5b835163c7600b45843f35fc5f5a
Jonathan Stowe <jns@gellyfish.co.uk>
2016-10-13 22:44:03 +0100

    Sketch the classes

--- lib/JSON/WSP.pm
+++ lib/JSON/WSP.pm
@@ -1,10 +1,27 @@
 use v6.c;
 
+use JSON::Class;
+
 module JSON::WSP {
     role Service does Callable {
         method CALL-ME($env) {
 
         }
+    }
+
+    class Specification {
+        has Str $.type;
+        has Version $.version;
+        has Str $.servicename;
+        has Str $.url;
+        has %.types;  
+        has %.methods;
+    }
+
+    class Request {
+    }
+
+    class Response {
     }
 
 
