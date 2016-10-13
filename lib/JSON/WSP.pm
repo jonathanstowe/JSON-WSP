010c38a1bd3d453e2199a80b427724ed173ececa
Jonathan Stowe <jns@gellyfish.co.uk>
2016-10-13 12:18:03 +0100

    Add some actual files

---
+++ lib/JSON/WSP.pm
@@ -0,0 +1,12 @@
+use v6.c;
+
+module JSON::WSP {
+    role Service does Callable {
+        method CALL-ME($env) {
+
+        }
+    }
+
+
+}
+# vim: expandtab shiftwidth=4 ft=perl6
---
+++ t/010-use.t
@@ -0,0 +1,11 @@
+#!/usr/bin/env perl6
+
+use v6.c;
+
+use Test;
+
+use-ok 'JSON::WSP', 'can use the module okay';
+
+
+done-testing;
+# vim: expandtab shiftwidth=4 ft=perl6
